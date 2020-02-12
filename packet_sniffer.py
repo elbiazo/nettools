8f987ffcae2f5ca507cfba7ccf0aaad8bee36908
ericbiazo <eric.l.biazo@gmail.com>
2020-02-12 15:54:59 -0800

    added ipv4 unpacker. Need to arch code better!

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,8 +1,12 @@
 import socket
 from networking.ethernet import Ethernet
+from networking.ipv4 import IPV4
 
 conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 while True:
   raw_data, addr = conn.recvfrom(65536)
   eth = Ethernet(raw_data)
-  eth.print_ethernet_frame()
+ #eth.print_ethernet_frame()
+  
+  ipv4 = IPV4(eth.raw_data)
+  ipv4.print_ipv4() 
